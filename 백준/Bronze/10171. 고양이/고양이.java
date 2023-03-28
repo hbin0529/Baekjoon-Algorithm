2461e66be61790ab654dc8619d6723e032847410
HYUNG BIN LEE <114133335+hbin0529@users.noreply.github.com>
2023-03-28 22:32:40 +0900

    [Bronze V] Title: 고양이, Time: 116 ms, Memory: 14024 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,17 @@
+public class Main {
+
+	public static void main(String[] args) {
+		/*
+			\    /\
+			 )  ( ')
+			(  /  )
+ 			 \(__)|
+		*/
+		
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+
+}
