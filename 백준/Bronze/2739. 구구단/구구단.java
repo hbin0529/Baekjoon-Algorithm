360321c8fa382e74d4e73839cc7f53183109d4ae
HYUNG BIN LEE <114133335+hbin0529@users.noreply.github.com>
2023-04-04 19:05:48 +0900

    [Bronze V] Title: 구구단, Time: 240 ms, Memory: 18560 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int sum = 0;
+		
+		for(int i = 1;  i <= 9; i++) {
+			sum = i * N;
+			System.out.println(N + " * " + i + " = " + sum);
+		}
+	}
+
+}
